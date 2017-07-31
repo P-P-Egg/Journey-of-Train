110978cd50356b74a081579eb9afbf97216ddc66
xiaomu1993 <30407753+xiaomu1993@users.noreply.github.com>
2017-07-31 22:40:55 +0800

    xiaomu
    1

---
+++ Assets/scrips/Onclickcontine.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Onclickcontine : MonoBehaviour {
+
+    public void OncoClicked()
+    {
+        SceneManager.LoadScene("scene_contine");
+    }
+}
---
+++ Assets/scrips/Onclickthanks.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Onclickthanks : MonoBehaviour {
+
+    public void OnthClicked()
+    {
+        SceneManager.LoadScene("scene_thanks");
+    }
+}
