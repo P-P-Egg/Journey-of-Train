5262a94a2c2ae0d85ea9029e8a65d87f2ddb896e
xiaomu1993 <30407753+xiaomu1993@users.noreply.github.com>
2017-10-02 02:40:09 +0800

    laozi

--- Assets/xiaomu_test/Scripts/bei_bao/bei_bao.cs
+++ Assets/xiaomu_test/Scripts/bei_bao/bei_bao.cs
@@ -4,16 +4,65 @@ using UnityEngine;
 
 public class bei_bao : MonoBehaviour {
 
-    List<int> bei_bao_list = new List<int>();
+    public static string wuping_name;
 
 
-    // Use this for initialization
+
+    public GameObject k_1;
+    public GameObject k_2;
+    public GameObject k_3;
+    public GameObject k_4;
+
+    public SpriteRenderer k_1_sp;
+    public SpriteRenderer k_2_sp;
+    public SpriteRenderer k_3_sp;
+    public SpriteRenderer k_4_sp;
+
+    List<Sprite> k_t_list = new List<Sprite>(); //框的图片list
+
+
+    public Sprite mao_1; //取得帽子1
+    public Sprite mao_2; //取得帽子2
+    public Sprite mao_3; //取得帽子3
+
+
     void Start () {
-		
-	}
+
+
+
+    }
 	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+        fu_zhi();
+
+
+    }
+
+    void suibian()
+    {
+        Sprite[] k_sp = { k_1_sp.sprite, k_2_sp.sprite, k_3_sp.sprite, k_4_sp.sprite };
+        k_t_list.AddRange(k_sp); //将几个框的sprit组件放入list
+    }
+
+    void fu_zhi()
+    {
+
+        if(wuping_name == "mao1")
+        {
+            k_1_sp.sprite = mao_1;
+            wuping_name = null;
+            Debug.Log(wuping_name);
+        }
+        if (wuping_name == "mao2")
+        {
+            wuping_name = null;
+        }
+        if (wuping_name == "mao3")
+        {
+            wuping_name = null;
+        }
+
+
+
+    }
 }
