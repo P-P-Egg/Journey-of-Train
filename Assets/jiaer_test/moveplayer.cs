a24eaae35dc56e7bc4ed18a31d299a91c7906643
505856727 <30371192+505856727@users.noreply.github.com>
2017-10-04 16:51:26 +0800

    jiaer

--- Assets/jiaer_test/moveplayer.cs
+++ Assets/jiaer_test/moveplayer.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class moveplayer : jiagou {
-
-	void Update () {
-        playermove();
-	}
+    private Vector3 mouseworldpos = new Vector3();
+    private Vector3 playerworldpos = new Vector3();
     public float speed = 20f;
+    void Update () {
+        playermove();
+	}   
     public void playermove()
     {
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetMouseButtonDown(0))
         {
-            this.transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            this.transform.Translate(Vector3.right * -speed * Time.deltaTime); 
-            
+            mouseworldpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            playerworldpos = GetComponent<Transform>().position;
+            print(mouseworldpos);
+            print(playerworldpos);
+            if (mouseworldpos.x > playerworldpos.x)
+            {
+                GetComponent<Transform>().Translate(speed * Time.deltaTime, 0, 0);
+            }
+            if(mouseworldpos.x < playerworldpos.x)
+            {
+                GetComponent<Transform>().Translate(-speed * Time.deltaTime, 0, 0);
+            }
         }
     }
 }
