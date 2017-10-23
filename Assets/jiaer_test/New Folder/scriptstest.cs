9ba7ac59c63cfe497986e856cba9cb080b2bdf22
505856727 <30371192+505856727@users.noreply.github.com>
2017-10-23 14:27:04 +0800

    jiaer

---
+++ Assets/jiaer_test/New Folder/scriptstest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptstest : MonoBehaviour
+{
+    public  GameObject[] boxlights=new GameObject[9];
+    private void Awake()
+    {
+        boxlights = GameObject.FindGameObjectsWithTag("boxlight");
+    }
+}
---
+++ Assets/jiaer_test/New Folder/timepiece.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timepiece : MonoBehaviour {
+    public string information = "这是一个手表，是老王十分喜爱的东西";
+}
