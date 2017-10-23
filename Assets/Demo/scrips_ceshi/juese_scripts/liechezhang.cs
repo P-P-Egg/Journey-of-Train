76233780fa51297214057fc236165de9dbaf1cc4
505856727 <30371192+505856727@users.noreply.github.com>
2017-10-23 22:03:48 +0800

    JIAER

---
+++ Assets/Demo/scrips_ceshi/juese_scripts/liechezhang.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class liechezhang : MonoBehaviour {
+    private Vector3 zhujue_pos = new Vector3();
+    private Vector3 npc_pos = new Vector3();
+    private Vector3 zhujue_sca = new Vector3();
+    private Vector3 npc_sca = new Vector3();
+    private GameObject zhujue;
+    private void Start()
+    {
+        zhujue = GameObject.FindWithTag("Player");
+    }
+    private void Update()
+    {
+        
+        zhujue_pos = zhujue.transform.position;
+        npc_pos = transform.position;
+        zhujue_sca = zhujue.transform.localScale;
+        npc_sca = transform.localScale;
+    }
+    private void OnMouseDown()
+    {
+        if (zhujue_pos.x > npc_pos.x)
+        {
+            zhujue.transform.localScale = new Vector3(-zhujue_sca.x, zhujue_sca.y, zhujue_sca.z);
+        }
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Demo/scrips_ceshi/juese_scripts/liechezhangdia.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using UnityEngine.UI;
+
+public class liechezhangdia : dialogue_jiagou {
+    private  dialogue_struct[] liechezhang=new dialogue_struct[8];
+    private int touch = 0;
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < 8; i++)
+        {
+            string[] role_detail_array = dialogues_list[i].Split(',');
+            liechezhang[i].ID = XmlConvert.ToInt32(role_detail_array[0]);
+            liechezhang[i].ischoose = XmlConvert.ToBoolean(role_detail_array[1]);
+            liechezhang[i].npc = role_detail_array[2];
+            liechezhang[i].cantalk = XmlConvert.ToBoolean(role_detail_array[3]);
+            liechezhang[i].istalk = XmlConvert.ToBoolean(role_detail_array[4]);
+            liechezhang[i].report = XmlConvert.ToBoolean(role_detail_array[5]);
+            liechezhang[i].content = role_detail_array[6];
+        }
+	}
+
+    private void OnMouseDown()
+    {
+        if (liechezhang[touch].ischoose == false)
+        {
+            GameObject.FindWithTag("dialogue").GetComponent<Text>().text = liechezhang[touch].content;
+            print(liechezhang[touch].content);
+        }
+        touch++;
+    }
+
+
+}
