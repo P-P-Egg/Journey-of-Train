104a1efe58a2def57af1652f30375728bfd75365
505856727 <30371192+505856727@users.noreply.github.com>
2017-10-24 23:20:17 +0800

    jiaer

---
+++ Assets/jiaer_test/New Folder/jubao.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class jubao : MonoBehaviour {
+    public GameObject[] jubaowupin = new GameObject[9];//举报物品
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        foreach(GameObject jubaowu in jubaowupin)
+        {
+            if (jubaowu.GetComponent<timepiece>().isjubao)
+            {
+                GameObject.FindWithTag("jubaoxinxi").GetComponent<Text>().text += jubaowu.GetComponent<timepiece>().npc+":\n"+jubaowu.GetComponent<timepiece>().information+"\n";
+            }
+        }
+    }
+}
