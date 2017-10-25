d2fd2896b300dc72ebd2604afad453c90f5dc15f
xiaomu1993 <30407753+xiaomu1993@users.noreply.github.com>
2017-10-25 09:08:11 +0800

    1

---
+++ Assets/Demo/Scrips/bei_bao/box/box1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box1 : MonoBehaviour {
+
+    private int box = 1; //几号包
+
+    public GameObject jubaolight;//高亮举报
+
+    public static bool box1_bool; //被举报的bool
+
+    void Start() {
+        
+    }
+
+    void Update() {
+
+        
+        if (transform.name == ju_bao1.ju_bao_name && box1_bool == false)
+        {
+            ju_bao_hou();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        zong_liang.box_name = transform.name; //全局等于名字，名字也是ID
+            
+        zong_liang.npc_box_int = box;
+
+        zong_liang.wu_ping_miao_su = "看上去是把锋利的砍刀，刀刃还透着白光。";
+
+    }
+
+
+    void ju_bao_hou()
+    {
+
+        jubaolight.GetComponent<Transform>().Translate(0, 0, -3f); //显示举报标记
+        box1_bool = true;
+        
+    }
+
+}
---
+++ Assets/Demo/Scrips/bei_bao/box/box3.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box3 : MonoBehaviour {
+
+    private int box = 3; //几号包
+
+    public GameObject jubaolight;//高亮举报
+
+    public static bool box3_bool; //被举报的bool
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (transform.name == ju_bao1.ju_bao_name && box3_bool == false)
+        {
+            ju_bao_hou();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+
+        zong_liang.box_name = transform.name; //全局等于名字，名字也是ID
+
+        zong_liang.npc_box_int = box;
+        zong_liang.wu_ping_miao_su = "看上去是把锋利的砍刀，刀刃还透着白光。";
+
+
+    }
+
+    void ju_bao_hou()
+    {
+
+        jubaolight.GetComponent<Transform>().Translate(0, 0, -3f); //显示举报标记
+        box3_bool = true;
+    }
+
+}
