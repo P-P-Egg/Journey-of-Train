3da927f13aaae9509e097e207f0e3154380a0f54
505856727 <30371192+505856727@users.noreply.github.com>
2017-10-26 19:27:38 +0800

    jiaer

---
+++ Assets/Demo/Scrips/bei_bao/jubaoscripts.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jubaoscripts : MonoBehaviour {
+
+    public int wrapcount = 0;//换行次数
+
+    private void Update()
+    {
+
+        print(wrapcount);
+    }
+    
+
+}
